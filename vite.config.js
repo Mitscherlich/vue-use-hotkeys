1d5876709d53a9436b580168ff72a8d738af1803
Mitscherlich Wei <mitscherlich36@gmail.com>
2022-04-20 00:47:15 +0800

    test: fix config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,4 @@
-import vue from '@vitejs/plugin-vue'
-import jsx from '@vitejs/plugin-vue-jsx'
-
 export default {
-  plugins: [vue(), jsx()],
   test: {
     globals: true,
     environment: 'jsdom',
